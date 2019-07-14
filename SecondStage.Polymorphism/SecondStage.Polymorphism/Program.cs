dfd6fb567f36ffe56a0a3352b2243db3fb3d5913
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-07-14 11:06:24 +0400

    Polymorphism

--- SecondStage.Polymorphism/SecondStage.Polymorphism/Program.cs
+++ SecondStage.Polymorphism/SecondStage.Polymorphism/Program.cs
@@ -12,22 +12,22 @@ namespace SecondStage.Polymorphism
         static void Main(string[] args)
         {
             User user = new User();
-
+            
             user.SignUp("Tigran", "123456");
             user.SignUp("Tigran", "123gh56");
             user.SignUp("Vahram", "787979");
             user.SignUp("Erik", "224716");
             user.SignUp("Robert", "174865");
             user.SignUp("Karlen", "175462");
-
+            
             Console.Write("\nLog in\nUsername: ");
             string username = Console.ReadLine();
-
+            
             Console.Write("Password: ");
             string password = Console.ReadLine();
-
+            
             UsersBase userLogin = user.LogIn(username, password);
-
+            
             if (userLogin != null)
             {
                 Console.ForegroundColor = ConsoleColor.Green;
@@ -39,15 +39,15 @@ namespace SecondStage.Polymorphism
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Wrong username or password");
             }
-
+            
             Console.ForegroundColor = ConsoleColor.White;
-
+            
             Console.WriteLine(new string('-',15));
-
+            
             UsersBase usersBase = user; // UpCast
             usersBase.Info();
-
-
+            
+            
             Console.WriteLine("Please input username and password for remove ");
             if (user.Remove()) // override method
             {
@@ -60,17 +60,17 @@ namespace SecondStage.Polymorphism
                 Console.WriteLine("Wrong username or password!!!");
             }
             Console.ForegroundColor = ConsoleColor.White;
-
+            
             Console.WriteLine("\n" + new string('-',15));
-
+            
             user = usersBase as User; // DownCast
             user.Info();
-
-
+            
+            
             user.InfoAUser("Vahram");
             Console.WriteLine(new string('-',15));
             user.InfoAUser(123546);
-
+            
             Console.ReadKey();
         }
     }

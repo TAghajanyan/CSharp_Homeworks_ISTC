43573216a75363af87ba6e8f032f3460eda9be24
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-08-19 12:19:51 +0400

    XMLConfiguration

---
+++ ThirdStage.XMLConfiguration/ThirdStage.XMLConfiguration/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Xml;
+using System.Configuration;
+using System.Collections;
+
+namespace ThirdStage.XMLConfiguration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var state = ConfigurationSettings.AppSettings;
+
+            Console.WriteLine("Key / Value\n------------");
+            foreach (string item in state)
+            {
+                Console.WriteLine(item + " -> " + state.Get(item));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

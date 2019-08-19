185b8e7b4b2d26ba7dc3c696bd542bfa99995c29
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-08-19 10:42:37 +0400

    Working with files

---
+++ ThirdStage.FileTree/ThirdStage.FileTree/FileTree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+
+namespace ThirdStage.FileTree
+{
+    class FileTree
+    {
+        public int FolderCount { get; private set; }
+        public int FileCount { get; private set; }
+
+        public void PrintFolders(DirectoryInfo directory, int pos = 0)
+        {
+            if (directory.FullName == string.Empty)
+                return;
+
+            PrintFiles(directory.FullName, pos + 3);
+
+            pos += 3;
+
+            for (int i = 0; i < directory.GetDirectories().Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(new string(' ', pos) + directory.GetDirectories()[i].Name);
+                FolderCount++;
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+                try
+                {
+                    PrintFolders(directory.GetDirectories()[i], pos);
+                }
+                catch (Exception e) when(e is PathTooLongException || e is UnauthorizedAccessException)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(e.Message);
+                }
+                catch (Exception e)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        public void PrintFiles(string filePath, int pos)
+        {
+            DirectoryInfo directory = new DirectoryInfo(filePath);
+
+            if (directory.GetFiles().Length == 0)
+                return;
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            foreach (FileInfo item in directory.GetFiles())
+            {
+                Console.WriteLine(new string(' ', pos) + item.Name);
+                FileCount++;
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
--- ThirdStage.FileTree/ThirdStage.FileTree/Program.cs
+++ ThirdStage.FileTree/ThirdStage.FileTree/Program.cs
@@ -5,58 +5,6 @@ namespace ThirdStage.FileTree
 {
     class Program
     {
-        static int folderCount = 0;
-        static int fileCount = 0;
-
-        static void PrintFolders(DirectoryInfo directory, int pos = 0)
-        {
-            if (directory.FullName == string.Empty)
-                return;
-
-            PrintFiles(directory.FullName, pos + 3);
-
-            pos += 3;
-
-            for (int i = 0; i < directory.GetDirectories().Length; i++)
-            {
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine(new string(' ', pos) + directory.GetDirectories()[i].Name);
-                folderCount++;
-                Console.ForegroundColor = ConsoleColor.Gray;
-
-                try
-                {
-                    PrintFolders(directory.GetDirectories()[i], pos);
-                }
-                catch (PathTooLongException e)
-                {
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine(e.Message);
-                }
-                catch (UnauthorizedAccessException e)
-                {
-                    Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.WriteLine(e.Message);
-                }
-            }
-        }
-
-        static void PrintFiles(string filePath, int pos)
-        {
-            DirectoryInfo directory = new DirectoryInfo(filePath);
-
-            if (directory.GetFiles() == null)
-                return;
-
-            Console.ForegroundColor = ConsoleColor.Red;
-            foreach (FileInfo item in directory.GetFiles())
-            {
-                Console.WriteLine(new string(' ', pos) + item.Name);
-                fileCount++;
-            }
-            Console.ForegroundColor = ConsoleColor.Gray;
-        }
-
         static void Main(string[] args)
         {
             string path = @"C:\";
@@ -69,10 +17,12 @@ namespace ThirdStage.FileTree
             Console.ForegroundColor = ConsoleColor.Gray;
             Console.WriteLine(new string('-', 20));
 
-            Console.WriteLine(path);
-            PrintFolders(directory);
+            FileTree fileTree = new FileTree();
 
-            Console.WriteLine("Files count: {0}\nFolders count: {1}", fileCount, folderCount);
+            Console.WriteLine(path);
+            fileTree.PrintFolders(directory);
+
+            Console.WriteLine("Files count: {0}\nFolders count: {1}", fileTree.FileCount, fileTree.FolderCount);
             Console.ReadKey();
         }
     }

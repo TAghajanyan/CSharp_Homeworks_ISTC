d33de778814b8048c5a7c52ba64c5252a7c42006
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-08-23 17:43:55 +0400

    GC, IDisposable

---
+++ ThirdStage._GC/ThirdStage._GC/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ThirdStage._GC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long memory = GC.GetTotalMemory(false) / 1024;
+
+            Console.WriteLine("Total memory in first: " + memory + "KB");
+
+            for (int i = 0; i < 10; i++)
+            { 
+                Console.WriteLine(i+1 + new string('-',25));
+
+                using (ResourceManager resource = new ResourceManager())
+                {
+                    resource.SetManagedResourceValue(out memory);
+                    Console.Write("\nTotal Memory [SetManagedResource]: " + memory + "KB" + " | ");
+                    Console.WriteLine("ManagedResource generation: " + GC.GetGeneration(resource.ManagedResource));
+
+                    resource.SetUnmanagedResourceValue(out memory);
+                    Console.Write("Total Memory [SetUnmanagedResource]: " + memory + "KB" + " | ");
+                    Console.WriteLine("UnmanagedResource generation: " + GC.GetGeneration(resource.UnmanagedResource));
+
+                    Thread.Sleep(1000);
+                }
+            }
+
+            Console.WriteLine($"\nGeneration 0 was scanned {GC.CollectionCount(0)} times");
+            Console.WriteLine($"Generation 1 was scanned {GC.CollectionCount(1)} times");
+            Console.WriteLine($"Generation 2 was scanned {GC.CollectionCount(2)} times");
+
+            memory = GC.GetTotalMemory(false) / 1024;
+
+            Console.WriteLine("\nTotal memory in end: " + memory + "KB");
+
+        }
+    }
+}
---
+++ ThirdStage._GC/ThirdStage._GC/ResourceManager.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace ThirdStage._GC
+{
+    public class ResourceManager : IDisposable
+    {
+        public FileStream UnmanagedResource;
+        public int[] ManagedResource;
+
+        public void SetManagedResourceValue(out Int64 memory)
+        {
+            ManagedResource = new int[1024*500]; // ~2MB
+            for (int i = 0; i < ManagedResource.Length; i++)
+            {
+                ManagedResource[i] = new int();
+            }
+            //GC.Collect();
+            Thread.Sleep(300);
+            memory = GC.GetTotalMemory(false) / 1024;
+        }
+
+        public void SetUnmanagedResourceValue(out Int64 memory)
+        {
+            UnmanagedResource = new FileStream(@"UnmanagedResourceFile.txt", FileMode.Open);
+            memory = GC.GetTotalMemory(false) / 1024;
+        }
+
+        private bool disposedValue = false;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposedValue)
+                return;
+
+            if (disposing)
+            {
+                UnmanagedResource.Dispose();
+            }
+
+            disposedValue = true;
+        }
+
+        ~ResourceManager()
+        {
+          Dispose(false);
+        }
+
+    }
+}

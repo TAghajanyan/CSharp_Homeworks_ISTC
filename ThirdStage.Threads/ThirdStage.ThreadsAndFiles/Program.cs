1d5dac5519093bc73f55a37df8fb79291b10cefe
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-08-27 02:46:38 +0400

    ThreadsAndFiles

--- ThirdStage.Threads/ThirdStage.ThreadsAndFiles/Program.cs
+++ ThirdStage.Threads/ThirdStage.ThreadsAndFiles/Program.cs
@@ -7,12 +7,10 @@ namespace ThirdStage.ThreadsAndFiles
     {
         static void Main(string[] args)
         {
-            //File.WriteAllText("File1.txt", "fdsfbsdafbdsfn");
-            //File.WriteAllText("File2.txt", "fdsfbsdafbdsfn");
-
-            ReadWriteWithThreads readWrite = new ReadWriteWithThreads("File1.txt", "File2.txt", "File3.txt", "File1Context", "File2Context", "File3Context");
+            ReadWriteWithThreads readWrite = new ReadWriteWithThreads
+                ("File1.txt", "File2.txt", "File3.txt", "File1Context", "File2Context", "File3Context");
             readWrite.Threads();
-            Thread.Sleep(2000);
+
         }
     }
 }
--- ThirdStage.Threads/ThirdStage.ThreadsAndFiles/ReadWriteWithThreads.cs
+++ ThirdStage.Threads/ThirdStage.ThreadsAndFiles/ReadWriteWithThreads.cs
@@ -1,15 +1,14 @@
 ﻿using System.IO;
+using System.Text;
 using System.Threading;
 
 namespace ThirdStage.ThreadsAndFiles
 {
     class ReadWriteWithThreads
     {
-        FileStream file1;
-        FileStream file2;
-        FileStream file3;
-
-        string path1, path2, path3;
+        string path1;
+        string path2;
+        string path3;
 
 
         public ReadWriteWithThreads(string path1, string path2, string context1, string context2)
@@ -17,52 +16,62 @@ namespace ThirdStage.ThreadsAndFiles
             this.path1 = path1;
             this.path2 = path2;
 
-            file1 = new FileStream(path1, FileMode.OpenOrCreate);
-            file2 = new FileStream(path2, FileMode.OpenOrCreate);
-            //try
-            //{
-                File.WriteAllText(path1, context1);
-                File.WriteAllText(path2, context2);
-            //}
-            //catch { }
+            FileStream file1 = new FileStream(path1, FileMode.OpenOrCreate);
+            FileStream file2 = new FileStream(path2, FileMode.OpenOrCreate);
+
+            byte[] buffer1 = GetBuffer(context1 + "\n");
+            byte[] buffer2 = GetBuffer(context2 + "\n");
+            file1.Write(buffer1, 0, buffer1.Length);
+            file2.Write(buffer2, 0, buffer2.Length);
+
+            file1.Close();
+            file2.Close();
         }
 
         public ReadWriteWithThreads(string path1, string path2, string path3, string context1, string context2, string context3) : this(path1, path2, context1, context2)
         {
             this.path3 = path3;
 
-            file3 = new FileStream(path3, FileMode.OpenOrCreate);
-            //try
-            //{
-                File.WriteAllText(path3, context3);
-            //}
-            //catch { }
+            FileStream file3 = new FileStream(path3, FileMode.OpenOrCreate);
+
+            byte[] buffer3 = GetBuffer(context3 + "\n");
+            file3.Write(buffer3, 0, buffer3.Length);
+
+            file3.Close();
         }
 
         private void ReadFiles(object path)
         {
-            try
+            StreamReader reader;
+            lock (this)
             {
-                lock (new object())
-                {
-                    StreamReader reader = new StreamReader((string)path);
-                    System.Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
-
-                    //Thread.CurrentThread.Join();
-                    System.Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
-                    WriteFile(reader.ReadToEnd());
-                }
-
+                reader = new StreamReader((string)path);
+                System.Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
             }
-            catch { }
+
+            if (Thread.CurrentThread.ManagedThreadId == 3)
+            {
+                Thread.Sleep(1);
+            }
+
+            lock (this)
+            {
+                System.Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                WriteFile(reader.ReadToEnd());
+                reader.Close();
+            }
         }
 
         private void WriteFile(string context)
         {
             System.Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
             File.AppendAllText(path3, context);
-            //Thread.CurrentThread.Join();
             System.Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private byte[] GetBuffer(string context)
+        {
+            return Encoding.UTF7.GetBytes(context);
         }
 
         public void Threads()
@@ -72,7 +81,7 @@ namespace ThirdStage.ThreadsAndFiles
 
             Thread thread2 = new Thread(ReadFiles);
             thread2.Start(path2);
-            thread2.Join();
+            //thread2.Join();
         }
 
     }

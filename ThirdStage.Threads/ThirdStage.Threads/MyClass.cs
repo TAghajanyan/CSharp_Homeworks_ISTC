6cbe5cf578d058f353a181121105a9155a74b68e
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-08-26 14:22:31 +0400

    Threads

---
+++ ThirdStage.Threads/ThirdStage.Threads/MyClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading;
+
+namespace ThirdStage.Threads
+{
+    class MyClass
+    {
+        int[] array = new int[45];
+        int a = 0, b = 1, counter = 0;
+
+        public void Print()
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                //Thread.Sleep(50);
+                Console.Write(array[i] + "   ");
+            }
+        }
+
+        public void Method()
+        {
+            while (counter < array.Length)
+            {
+                object obj = new object();
+
+                lock (obj) // Comment line
+                {
+                    array[counter] = a + b;
+                    a = b;
+                    b = array[counter];
+
+                    counter++;
+
+                    Thread thread = new Thread(Method);
+                    thread.Start();
+                    Thread.Sleep(50);
+                }
+            }
+        }
+    }
+}
---
+++ ThirdStage.Threads/ThirdStage.Threads/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading;
+
+namespace ThirdStage.Threads
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+
+            Thread thread = new Thread(myClass.Method);
+            thread.Start();
+            //thread.Join();
+            Thread.Sleep(50);
+
+            myClass.Print();
+        }
+    }
+}

fdddc4b63ee23ac7dba42c52eb0ec519db0024e4
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-08-28 17:08:53 +0400

    Semaphore

---
+++ AsyncModelProgramming/CriticalSections/_Semaphore/01_Semaphore/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _01_Semaphore
+{
+    class Program
+    {
+        static Semaphore semaphore;
+
+        static void Method()
+        {
+            semaphore.WaitOne();
+            Console.WriteLine("Start[CurrentThread ID: {0}]\n", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(1200);
+            Console.WriteLine("Finish[CurrentThread ID: {0}]\n", Thread.CurrentThread.ManagedThreadId);
+            semaphore.Release(1);
+        }
+
+        static void Main(string[] args)
+        {
+            semaphore = new Semaphore(6,11);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread thread = new Thread(Method);
+                thread.Start();
+            }
+        }
+    }
+}

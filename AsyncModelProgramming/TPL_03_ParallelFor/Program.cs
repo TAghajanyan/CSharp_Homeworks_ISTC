19b1cda7cf0c44f4b36f61a3149a9472a2830fd9
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-08-28 14:07:58 +0400

    ParallelFor, ParallelLoopState

---
+++ AsyncModelProgramming/TPL_03_ParallelFor/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TPL_03_ParallelFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch timer = new Stopwatch();
+
+            ParallelOptions options = new ParallelOptions();
+            options.MaxDegreeOfParallelism = 1;
+
+            int[] arr = new int[1_000_000];
+            Action<int> action = (int x) => arr[x] = x * x * x;
+
+
+            timer.Start();
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = i;
+            timer.Stop();
+
+            Console.WriteLine("Simple For {0} second.", timer.Elapsed.TotalSeconds);
+
+
+            timer.Reset();
+            timer.Start();
+            Parallel.For(0, arr.Length, options, action);
+            timer.Stop();
+
+            Console.WriteLine("ParallelFor [Logical processors]: {0} || {1} second.", options.MaxDegreeOfParallelism, timer.Elapsed.TotalSeconds);
+
+            //------------------------------------------
+
+            options.MaxDegreeOfParallelism = 2;
+
+            timer.Reset();
+            timer.Start();
+            Parallel.For(0, arr.Length, options, action);
+            timer.Stop();
+
+            Console.WriteLine("ParallelFor [Logical processors]: {0} || {1} second.", options.MaxDegreeOfParallelism, timer.Elapsed.TotalSeconds);
+
+            //------------------------------------------
+
+            options.MaxDegreeOfParallelism = 4;
+
+            timer.Reset();
+            timer.Start();
+            Parallel.For(0, arr.Length, options, action);
+            timer.Stop();
+
+            Console.WriteLine("ParallelFor [Logical processors]: {0} || {1} second.", options.MaxDegreeOfParallelism, timer.Elapsed.TotalSeconds);
+
+            //------------------------------------------
+
+
+            int[] arr1 = new int[1_000_000];
+
+            //arr1[200] = -1;
+
+            Action<int, ParallelLoopState> action1 = (int i, ParallelLoopState state) =>
+             {
+                 if (arr1[i] < 0)
+                 {
+                     state.Break();
+                 }
+
+                 arr1[i] = i;
+             };
+
+            timer.Reset();
+            timer.Start();
+            var res = Parallel.For(0, arr1.Length, options, action1);
+            timer.Stop();
+
+            if (res.IsCompleted)
+            {
+                Console.WriteLine("\nParallelFor with ParallelLoopState [Logical processors]: {0} || {1} second.", options.MaxDegreeOfParallelism, timer.Elapsed.TotalSeconds);
+            }
+            else
+            {
+                Console.WriteLine($"Loop is stoped.\n Item index: {res.LowestBreakIteration}");
+            }
+
+
+
+        }
+    }
+}

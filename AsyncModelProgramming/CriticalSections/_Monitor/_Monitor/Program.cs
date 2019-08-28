eba632adf71c5cb82ef247a22c1d3a367bdbd2fa
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-08-28 17:19:12 +0400

    Monitor

---
+++ AsyncModelProgramming/CriticalSections/_Monitor/_Monitor/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _Monitor
+{
+    class Program
+    {
+        static object obj = new object();
+
+        static void Method()
+        {
+            Monitor.Enter(obj);
+            Console.WriteLine("Start[CurrentThread ID: {0}]", Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(1200);
+            Console.WriteLine("Finish[CurrentThread ID: {0}]", Thread.CurrentThread.ManagedThreadId);
+            Monitor.Exit(obj);
+
+            // Or-------------------------------
+
+            //lock (obj)
+            //{
+            //    Console.WriteLine("Start[CurrentThread ID: {0}]", Thread.CurrentThread.ManagedThreadId);
+            //    Thread.Sleep(1200);
+            //    Console.WriteLine("Finish[CurrentThread ID: {0}]", Thread.CurrentThread.ManagedThreadId);
+            //}
+        }
+
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[5];
+
+            for (int i = 0; i < threads.Length; i++)
+            {
+                threads[i] = new Thread(Method);
+                threads[i].Start();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}

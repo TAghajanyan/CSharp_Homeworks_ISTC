a926543872cb20ca5c085eda4804b1f5a70daad5
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-08-28 16:14:06 +0400

    Mutex

---
+++ AsyncModelProgramming/CriticalSections/_Mutex/02_Mutex/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _02_Mutex
+{
+    class Program
+    {
+        static Mutex mutex = new Mutex(false, "MyMutex");
+
+        static void Method1()
+        {
+            //mutex.WaitOne();
+            Console.WriteLine("[Method1]Start. CurrentThread ID: " + Thread.CurrentThread.ManagedThreadId);
+            Method2();
+            //mutex.ReleaseMutex();
+            Console.WriteLine("[Method1]Finish. CurrentThread ID: " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static void Method2()
+        {
+            //mutex.WaitOne();
+            Console.WriteLine("[Method2]Start. CurrentThread ID: " + Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(1000);
+            //mutex.ReleaseMutex();
+            Console.WriteLine("[Method2]Finish. CurrentThread ID: " + Thread.CurrentThread.ManagedThreadId);
+        }
+
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[5];
+            for (int i = 0; i < 5; i++)
+            {
+                threads[i] = new Thread(Method1);
+                threads[i].Start();
+                threads[i].Join(); //another way, when commented all mutex methods
+            }
+        }
+    }
+}
---
+++ AsyncModelProgramming/CriticalSections/_Mutex/_Mutex/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _Mutex
+{
+    class Program
+    {
+
+        //static Mutex mutex = new Mutex(); //all app work simultaneously
+        static Mutex mutex = new Mutex(false, "Mutex1"); //all app work sequence
+
+        static void Main(string[] args)
+        {
+            Thread[] threads = new Thread[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                threads[i] = new Thread(Method);
+                //Thread.Sleep(500);
+                threads[i].Start();
+            }
+        }
+
+        static void Method()
+        {
+            mutex.WaitOne();
+
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            Thread.Sleep(1000);
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId + "\n");
+
+            mutex.ReleaseMutex();
+        }
+    }
+}

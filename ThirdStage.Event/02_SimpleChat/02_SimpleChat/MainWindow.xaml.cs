b3fe2cc4aab8ab6085b1635927ea741925d1be10
Tigran Aghajanyan <taghajanyan99@gmail.com>
2019-09-02 18:15:00 +0400

    SimpleChat

---
+++ ThirdStage.Event/02_SimpleChat/02_SimpleChat/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02_SimpleChat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void B1_Click(object sender, RoutedEventArgs e)
+        {
+            if (TBox1.Text != string.Empty)
+            {
+                TBlock1.Text += new string(' ', 50) + "Me: " + TBox1.Text + Environment.NewLine;
+                TBlock2.Text += "User1: " + TBox1.Text + Environment.NewLine;
+                TBox1.Text = string.Empty;
+            }
+        }
+
+        private void B2_Click(object sender, RoutedEventArgs e)
+        {
+            if (TBox2.Text != string.Empty)
+            {
+                TBlock1.Text += "User2: " + TBox2.Text + Environment.NewLine;
+                TBlock2.Text += new string(' ', 50) + "Me: " + TBox2.Text + Environment.NewLine;
+                TBox2.Text = string.Empty;
+            }
+        }
+    }
+}
